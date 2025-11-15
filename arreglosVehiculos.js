0729d3f672332ebc000c82fa8d89847064d38129
AVMP277G <angelmarpo007@gmail.com>
2025-11-15 17:09:22 -0500

    Add vehicle registration and logging functionality
    This script initializes an array of vehicles and logs their details to the console.

---
+++ arreglosVehiculos.js
@@ -0,0 +1,17 @@
+let vehiculos = [];
+vehiculos.push({
+  tipo: "Camión",
+  capacidad: "15-25 ton",
+  material: "Arena",
+  horas: "8-12 h/día",
+  construccion: "Vial"
+});
+//Resultado en consola:
+console.log("Vehículos registrados:");
+console.log(vehiculos);
+// Mostrar cada campo
+console.log("Tipo:", vehiculos[0].tipo);
+console.log("Capacidad:", vehiculos[0].capacidad);
+console.log("Material:", vehiculos[0].material);
+console.log("Horas:", vehiculos[0].horas);
+console.log("Construcción:", vehiculos[0].construccion);
