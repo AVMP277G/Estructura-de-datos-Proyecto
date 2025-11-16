c4a21e5921414092a6085a530dd9fa76914c2b37
AVMP277G <angelmarpo007@gmail.com>
2025-11-16 11:10:47 -0500

    Remove comments from pilaHistorial.js
    Removed comments from the pilaHistorial.js file.

--- pilaHistorial.js
+++ pilaHistorial.js
@@ -1,23 +1,18 @@
 let pilaHistorial = [];
 
-// Operación push
 pilaHistorial.push("Vehículo registrado: Camión");
 pilaHistorial.push("Mantenimiento agregado");
 pilaHistorial.push("Reporte generado");
 
-// Mostrar pila completa
 console.log("Pila actual:");
 console.log(pilaHistorial);
 
-// Operación peek (ver el último elemento)
 console.log("Última acción realizada:");
 console.log(pilaHistorial[pilaHistorial.length - 1]);
 
-// Operación pop (deshacer última acción)
 console.log("Deshaciendo acción...");
 let accionDeshecha = pilaHistorial.pop();
 console.log("Acción eliminada:", accionDeshecha);
 
-// Mostrar pila luego de pop
 console.log("Pila después del pop:");
 console.log(pilaHistorial);
